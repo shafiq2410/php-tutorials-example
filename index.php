db20e47b2b3f96c9f23be43b2948c76c1ca535d6
Md. Shafiqul Islam <shafiq2410@gmail.com>
2020-04-11 22:16:29 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Programming schools";
+$heading_message ="Welcome to ProgSchools.com";
+$sub_heading_message= "Learn programming language schools from professional software engineers";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php $title; ?></title>
+<link href="style.css" rel = "stylesheet" />
+</head>
+<body>
+
+<h1><?php echo $heading_message; ?></h1>
+<hr>
+<h4> <i><?php echo $sub_heading_message; ?> </i></h4>
+<hr>
+<p>&copy;2020. all right reserved.</p>
+</body>
+</html> 
